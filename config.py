a18a35ebb126303865c11abaf78d263e64c19d02
Hamza99Mu <muhammadhamza4949@gmail.com>
2026-09-14 00:08:12 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,24 @@
 import os
 import streamlit as st
 
-def get_secret(name: str, default: str = "") -> str:
+def get_secret(key: str, default=None):
+    val = None
     try:
-        value = st.secrets.get(name)
-        if value:
-            return str(value)
+        val = st.secrets.get(key)
     except Exception:
         pass
-    return os.getenv(name, default)
+    if not val:
+        try:
+            for sec in st.secrets.values():
+                if isinstance(sec, dict) and key in sec:
+                    val = sec[key]
+        except Exception:
+            pass
+    if not val:
+        val = os.getenv(key, default)
+    
+    if val:
+        os.environ[key] = str(val)
+    return val
+
+get_secret("GROQ_API_KEY")
